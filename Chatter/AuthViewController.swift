9829a9e0cb2f651da1dcaf5e0f5cf2a88293bc95
Olga Mikhnenko <olga230397@icloud.com>
2020-11-24 14:29:14 +0300

    Added canvas editing

---
+++ Chatter/AuthViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  Chatter
+//
+//  Created by Helga on 24.11.2020.
+//
+
+/*import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}*/
+
+import UIKit
+
+class AuthViewController: UIViewController {
+    
+    /*let logoImageView = UIImageView(image: #imageLiteral(resourceName: "Logo"), contentMode: .scaleAspectFit)
+    
+    let googleLabel = UILabel(text: "Get started with")
+    let emailLabel = UILabel(text: "Or sign up with")
+    let alreadyOnboardLabel = UILabel(text: "Alerady onboard?")
+    
+    let googleButton = UIButton(title: "Google", titleColor: .black, backgroundColor: .white, isShadow: true)
+    let emailButton = UIButton(title: "Email", titleColor: .white, backgroundColor: .buttonDark())
+    let loginButton = UIButton(title: "Login", titleColor: .buttonRed(), backgroundColor: .white, isShadow: true)*/
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
+
+// MARK: - SwiftUI
+import SwiftUI
+
+struct AuthVCProvider: PreviewProvider {
+    static var previews: some View {
+        ContainerView().edgesIgnoringSafeArea(.all)
+    }
+    
+    struct ContainerView: UIViewControllerRepresentable {
+        
+        let viewController = AuthViewController()
+        
+        func makeUIViewController(context: UIViewControllerRepresentableContext<AuthVCProvider.ContainerView>) -> AuthViewController {
+            return viewController
+        }
+        
+        func updateUIViewController(_ uiViewController: AuthVCProvider.ContainerView.UIViewControllerType, context: UIViewControllerRepresentableContext<AuthVCProvider.ContainerView>) {
+            
+        }
+    }
+}
+
+
+
